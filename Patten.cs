3ed2c4959f463268c45df1cfd7dde0984be3af56
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-16 05:13:44 +0530

    Anurag Prakash ; has added

---
+++ Patten.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace RegexPatternMatchingDemo
+{
+    class Patterns
+    {
+        public static string Regex_First_Name = "^[A-Z][a-z]{2,}$";
+        public static string Regex_Last_Name = "^[A-Z][a_z]{2,}$";
+
+
+
+        public bool validateFirstName(string name)
+        {
+            return Regex.IsMatch(name, Regex_First_Name);
+        }
+        public bool validateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, Regex_Last_Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RegexPatternMatchingDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Patterns firstName = new Patterns();
+            Patterns lastName = new Patterns();
+            Console.WriteLine(firstName.validateFirstName("Anurag"));
+            Console.WriteLine(lastName.validateFirstName("Prakash"));
+            Console.ReadKey();
+
+        }
+    }
+}
