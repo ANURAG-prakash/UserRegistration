e89370e31c62ed696541cb17b2432116d251c94c
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-16 05:54:02 +0530

    Anurag prakash ; has Added

---
+++ Patten.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace RegexPatternMatchingDemo
+{
+    class Patterns
+    {
+        public static string Regex_First_Name = "^[A-Z][a-z]{2,}$";
+        public static string Regex_Last_Name = "^[A-Z][a_z]{2,}$";
+        public static string Regex_Email = "^[a - z0 - 9A - Z] + ([._ + -][a - z0 - 9A - Z] +) *[@][a - z0 - 9A - Z] +[.][a - zA - Z]{2,3}(.[a-zA-Z]{2})?$";
+        public static string Regex_Mobile = "^[0-9]{2}[6-9]{1}[0-9]{9}$";
+        public static string Regex_Password = "^.*(?=.*[a-z0-9])*(?=.*[A-Z])*(?=.*[!@#$%^&*])(.{9,})$";
+        public bool validateFirstName(string name)
+        {
+            return Regex.IsMatch(name, Regex_First_Name);
+        }
+        public bool validateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, Regex_Last_Name);
+        }
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
+        }
+        public bool validateMobile(string mobile)
+        {
+            return Regex.IsMatch(mobile, Regex_Mobile);
+        }
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace RegexPatternMatchingDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Patterns firstName = new Patterns();
+            Patterns lastName = new Patterns();
+            Patterns email = new Patterns();
+            Patterns mobile = new Patterns();
+            Patterns password = new Patterns();
+            Console.WriteLine(firstName.validateFirstName("Anurag"));
+            Console.WriteLine(lastName.validateLastName("Prakash"));
+            Console.WriteLine(email.validateEmail("prakash.shanu80@gmail.com"));
+            Console.WriteLine(mobile.validateMobile("9518039211"));
+            Console.WriteLine(password.validateFirstName("@Anurag123"));
+            Console.ReadKey();
+
+        }
+    }
+}
