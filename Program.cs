9bb243624b59f53cc18dfb32d9aea5afca4a5364
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-16 05:41:11 +0530

    Added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace RegexPatternMatchingDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Patterns firstName = new Patterns();
+            Patterns lastName = new Patterns();
+            Patterns email = new Patterns();
+            Patterns mobile = new Patterns();
+            Console.WriteLine(firstName.validateFirstName("Anurag"));
+            Console.WriteLine(lastName.validateFirstName("Prakash"));
+            Console.WriteLine(email.validateFirstName("prakash.shanu80@gmail.com"));
+            Console.WriteLine(email.validateFirstName("9518039211"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
